7c8509595c7f04c918e08c17ad23097f9777aca5
unknown <sheffalee123@gmail.com>
2023-06-21 00:36:09 +0530

    my commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <h3>Made with <span className='red'><i class="fa-solid fa-heart"></i></span></h3>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
