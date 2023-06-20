a35721354862924ddcca2414ddfeeb83d3caf63b
unknown <sheffalee123@gmail.com>
2023-06-21 00:38:20 +0530

    my commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
+      <h1>Made by sheffalee</h1>
       <h3>Made with <span className='red'><i class="fa-solid fa-heart"></i></span></h3>
     </div>
   )
