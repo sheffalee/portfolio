d9eeffd84504618be919806467c4172a07a9e8c6
unknown <sheffalee123@gmail.com>
2023-06-21 00:26:14 +0530

    my commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This is footer </h1>
+    <div className='footer'>
+      
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,28 +8,28 @@ const Projects = () => {
       title: 'Project 1',
       description: 'The Word Counter app in ReactJS allows users to easily count words in text, providing a user-friendly interface and instant word count display. It includes additional features like character count and a progress bar for efficient tracking of text length. A valuable tool for writers and editors to monitor word usage.',
       imageUrl: 'word-counter.jpg',
-      url: 'Link'
+      url: 'https://sheffalee.github.io/word-counter-app/'
     },
     {
       id: 2,
       title: 'Project 2',
       description: 'The Calculator app is a ReactJS application that provides users with a user-friendly interface to perform basic arithmetic operations. It supports addition, subtraction, multiplication, and division, allowing users to input numbers and obtain instant results. With its simple design and efficient functionality, the Calculator app is a handy tool for quick calculations.',
       imageUrl: 'calculator.jpg',
-      url: 'Link'
+      url: 'https://sheffalee.github.io/calculator-app/'
     },
     {
       id: 3,
       title: 'Project 3',
       description: 'The Color Picker app in ReactJS enables users to explore and select colors from a wide range of options. With an intuitive user interface, users can easily preview and choose colors, obtaining their respective hex codes. This app is a valuable tool for designers and developers seeking efficient color selection and integration into their projects.',
       imageUrl: 'color-picker.png',
-      url: 'Link'
+      url: 'https://sheffalee.github.io/color-picker-app/'
     },
     {
       id: 4,
       title: 'Project 4',
       description: 'The Age Calculator app in ReactJS allows users to quickly calculate their age based on their birthdate. It provides a user-friendly interface where users can input their birthdate and instantly view their age in years, months, and days. This app is a handy tool for anyone who wants to determine their age accurately and conveniently.',
       imageUrl: 'agecalc.png',
-      url: 'Link'
+      url: 'https://sheffalee.github.io/age-calculator-app/'
     }
   ];
 
