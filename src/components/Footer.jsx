d6e268005cd431d8799368b40fa496e00b36b4cc
unknown <sheffalee123@gmail.com>
2023-06-21 16:03:00 +0530

    my commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import './Footer.css';
 const Footer = () => {
   return (
+    // <div className='footer'>
+    //   <h1>Made by sheffalee</h1>
+    //   <h3>Made with <span className='red'><i class="fa-solid fa-heart"></i></span></h3>
+    // </div>
     <div className='footer'>
-      <h1>Made by sheffalee</h1>
-      <h3>Made with <span className='red'><i class="fa-solid fa-heart"></i></span></h3>
+      <h4>Made with </h4>
+      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"></svg>
     </div>
   )
 }
