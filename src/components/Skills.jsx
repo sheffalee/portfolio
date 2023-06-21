eadcd92a3c49262d46f68bd6fe6c1ecb80c008a6
unknown <sheffalee123@gmail.com>
2023-06-21 18:35:25 +0530

    my commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,11 +8,11 @@ const Skills = () => {
         <img className='image-1' src='./html.png'></img>
         <img className='image-2' src='./css.png'></img>
         <img className='image-3' src='./js.png'></img>
-        <img className='image-8' src='./bootstrap.png'></img>
-        <img className='image-4' src='./react.png'></img>
-        <img className='image-5' src='./nodejs.png'></img>
-        <img className='image-6' src='./mongodb.png'></img>
-        <img className='image-7' src='./express.pnj'></img>
+        <img className='image-4' src='./bootstrap.png'></img>
+        <img className='image-5' src='./react.png'></img>
+        <img className='image-6' src='./nodejs.png'></img>
+        <img className='image-7' src='./mongodb.png'></img>
+        <img className='image-8' src='./express.pnj'></img>
     </div>
   )
 }
